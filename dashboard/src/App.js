e28de4da00bdd4ea0be11d4c55a44c66d765397e
oceansalle <oceanesalleiae@gmail.com>
2022-05-06 12:40:15 +0200

    maindash component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import MainDash from './components/MainDash';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div>
-      
+      <Sidebar />
+      <MainDash />
     </div>
   );
 }
