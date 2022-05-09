617fb8a2eeea3af8bddec5e7623d0539a7fc5ae9
oceansalle <oceanesalleiae@gmail.com>
2022-05-09 11:41:45 +0200

    Cards component

--- dashboard/src/components/MainDash.js
+++ dashboard/src/components/MainDash.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Cards from "../Cards/Cards";
-import Table from "../Table/Table";
+import Cards from "../components/Card.js";
+import Table from "../components/Table";
 import "./MainDash.css";
 const MainDash = () => {
   return (
